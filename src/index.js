5b8fcc986c8d9d6fdb46f9f660cb34cb3e594294
Kyle Chin <kyle.chin@sterlingcapitalbrokers.com>
2018-02-15 12:51:45 -0500

    setup handlebars, express.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+require('handlebars');
+
+var things = require('./things.js');
+
+//handlebars
+// path = require('path');
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+	}));
+app.set('view engine', 'handlebars');
+
+
+
 app.get('/', function (req, res) {
-    res.send('Hello from Docker\n');
+    res.render('hello');
 });
+
+app.use('/things', things);
 
 app.listen(8080);
 console.log('Running on http://localhost:8080');
