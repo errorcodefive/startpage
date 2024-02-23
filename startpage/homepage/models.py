ea50c58bc11f9440b041420fff0695177bee6c38
Kyle Chin <kylechin@errorcodefive.com>
2024-02-23 13:19:39 -0500

    Working on templates

--- startpage/homepage/models.py
+++ startpage/homepage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Bookmarks(models.Model):
+    bookmark_name = models.CharField(max_length = 50)
+    bookmark_link = models.CharField(max_length = 200, help_text = "URL")
+    bookmark_order = models.IntegerField(help_text = "Display Order")
+
+    def __str__(self):
+        return str(self.bookmark_name)
